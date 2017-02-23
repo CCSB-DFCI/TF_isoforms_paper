81564a426d0304cf681fc84f97b34c527550c675
Andrew Goldfarb <andrewgoldfarb@piaguest-10-239-187-161.piaguest.partners.org>
2017-02-23 10:13:05 -0500

    Uploading initial mutational mapping pipeline.

---
+++ mutation_mapping_andrew/HGMD_pointmutations.py
@@ -0,0 +1,21 @@
+"""
+Andrew Goldfarb
+02/21/17
+Filters "HGMD_allmut.tsv" to include only missense/nonsense mutations.
+"""
+
+HGMD_file = open("HGMD_allmut.tsv", "r")
+
+HGMD_lines = []
+for line in HGMD_file:
+	temp_list = line.split("\t")
+	if temp_list[7] == "NULL" and temp_list[8] == "NULL" and temp_list[6] != "NULL":
+		HGMD_lines.append(line)
+# Keeping lines that have "NULL" in insertion and deletion information, but not amino acid change position.
+
+#Create a  file, HGMD_pointmutations, containing the CDS lines.
+final_fasta = open("HGMD_pointmutations.txt", "w")
+for i in HGMD_lines:
+	i = i.strip()
+	final_fasta.write(i + "\n")
+final_fasta.close()
