c032f06cf8462b917e46c548168fd1655cfca208
gsheynkman <gloriam_sheynkman@dfci.harvard.edu>
2017-02-27 15:02:53 -0500

    Added pickle if statement.

--- mutation_mapping_andrew/data_structure.py
+++ mutation_mapping_andrew/data_structure.py
@@ -5,6 +5,7 @@ Task: Create a data structure that organizes the "gencode.v25.annotation.gtf" fi
 """
 import pickle
 import os
+import functions
 
 # sublime text editor runs from active directory in finder
 # make working directory one where script is located
@@ -12,83 +13,81 @@ abspath = os.path.abspath(__file__)
 dname = os.path.dirname(abspath)
 os.chdir(dname)
 
-# Create the data structure and fill it with information from "gencode.v25.annotation.gtf"
-d = {}
-# for line in open("./data/gencode.v25.annotation.gtf", "r"):
-for line in open("./toy.gtf"):
-	print line
-	if line[0] != "#":
-		fields = line.strip().split("\t")
-		chrom, annot_source, feature, start, end, dot1, strand, dot2, flag = fields
+d_pickle = 'toy_gtf_dict.p'
+if not os.path.exists(d_pickle):
+	# Create the data structure and fill it with information from "gencode.v25.annotation.gtf"
+	d = {}
+	# for line in open("./data/gencode.v25.annotation.gtf", "r"):
+	for line in open("./toy.gtf"):
+		if line[0] != "#":
+			fields = line.strip().split("\t")
+			chrom, annot_source, feature, start, end, dot1, strand, dot2, flag = fields
 
-		if feature == "gene" and "protein_coding" in flag:
-			words = flag.split(" ")
-			ENSG = words[1][1:-2]
-			gene_name = words[7][1:-2]
-			if ENSG not in d:
-				d[ENSG] = [gene_name, chrom, {"start": start, "end": end}, {}]
+			if feature == "gene" and "protein_coding" in flag:
+				words = flag.split(" ")
+				ENSG = words[1][1:-2]
+				gene_name = words[7][1:-2]
+				if ENSG not in d:
+					d[ENSG] = [gene_name, chrom, {"start": start, "end": end}, {}]
 
-		elif feature == "transcript" and "protein_coding" in flag:
-			words = flag.split(" ")
-			ENST = words[3][1:-2]
-			ENSG = words[1][1:-2]
-			if ENSG in d:
-				ensts = d[ENSG][3]
-				if ENST not in ensts:
-					d[ENSG][3][ENST] = [strand, {"start": start, "end": end}, {}]
-				else:
-					print "error"
+			elif feature == "transcript" and "protein_coding" in flag:
+				words = flag.split(" ")
+				ENST = words[3][1:-2]
+				ENSG = words[1][1:-2]
+				if ENSG in d:
+					ensts = d[ENSG][3]
+					if ENST not in ensts:
+						d[ENSG][3][ENST] = [strand, {"start": start, "end": end}, {}]
+					else:
+						print "error"
 
-		elif feature == "CDS" and "protein_coding" in flag:
-			words = flag.split(" ")
-			CDS_index = int(words[17][0:-1])
-			ENST = words[3][1:-2]
-			ENSG = words[1][1:-2]
-			if ENSG in d:
-				cdss = d[ENSG][3][ENST][2]
-				if CDS_index not in cdss:
-					d[ENSG][3][ENST][2][CDS_index] = ["", {"abs_start": start, "abs_end": end}, {}]
+			elif feature == "CDS" and "protein_coding" in flag:
+				words = flag.split(" ")
+				CDS_index = int(words[17][0:-1])
+				ENST = words[3][1:-2]
+				ENSG = words[1][1:-2]
+				if ENSG in d:
+					cdss = d[ENSG][3][ENST][2]
+					if CDS_index not in cdss:
+						d[ENSG][3][ENST][2][CDS_index] = ["", {"abs_start": start, "abs_end": end}, {}]
 
-#Function for getting reverse complement of a sequence
-def reverse_complement(sequence):
-    
-    Complements = {"A":"T", "C":"G", "G":"C", "T":"A"}
+	pickle.dump(d, open('toy_gtf_dict.p', 'wb'))
+else:
+	d = pickle.load(open('toy_gtf_dict.p'))
 
-    reverse_seq = ""
-    for s in reversed(sequence):
-        reverse_seq += Complements.get(s, s)
-    return reverse_seq
-#CDS = "ATGAGCACAGGCCTGCGGTACAAGAGCAAGCTGGCGACCCCAGGTGAGGACAAGCAGGTAGACATTGACAAGCAGTACGTGGGCTTCGCCACACTGCCCAACCAGGTGCACCGCAAGTCGGTGAAGAAAGGCTTTGACTTCACACTCATGGTGGCTGGTGGTGAGTCAGGCCTGGGGAAGTCCACACTGGTCCACAGCCTCTTCCTGACAGACTTGTACAAGGACCGGAAGCTGCTCAGTGCTGAGGGTGAGCGCATCAGCCAGACGGTAGAGATTCTAAAACACACGGTGGACATTGAGGAGAAGGGAGTCAAGCTGAAGCTCACCATCGTGGACACGCCGGGATTCGGGGACGCTGTCAACAACACCGAGTGGTGCTGGAAGCCCATCACCGACTATGTGGACCAGCAGTTTGAGCAGTACTTCCGTGATGAGAGCGGCCTCAACCGAAAGAACATCCAAGACAACCGAGTGCACTGCTGCCTATACTTCATCTCCCCCTTCGGGCATGGGTGGCTGCGGCCAGTGGATGTGGGTTTCATGAAGGCATTGCATGAGAAGGTCAACATCGTGCCTCTCATCGCCAAAGCTGACTGTCTTGTCCCCAGTGAGATCCGGAAGCTGAAGGAGCGGGTGATCCGGGAGGAGATTGACAAGTTTGGGATCCATGTATACCAGTTCCCTGAGTGTGACTCGGACGAGGATGAGGACTTCAAGCAGCAGGACCGGGAACTGAAGGTGGAGAGCGCGCCCTTCGCCGTTATAGGCAGCAACACGGTGGTGGAGGCCAAGGGGCAGCGGGTCCGGGGCCGACTGTACCCCTGGGGGATCGTGGAGGGTGTGGAGAACCAGGCGCATTGCGACTTCGTGAAGCTGCGCAACATGCTCATCCGCACGCATATGCACGACCTCAAGGACGTGACGTGCGACGTGCACTACGAGAACTACCGCGCGCACTGCATCCAGCAGATGACCAGGTGCAAACTGACCCAGGACAGCCGCATGGAGAGCCCCATCCCGATCCTGCCGCTGCCCACCCCGGACGCCGAGACTGAGAAGCTTATCAGGATGAAGGATGAGGAAGTACTGAGGCGCATGCAGGAGATGCTGCAGAGGATGAAGCAGCAGATGCAGGACCAGTGA"
-#print reverse_complement(CDS)
 
-#Extract CDS sequences from hg38 and add to data structure
-#MAKE INTO A FUNCTION! INPUT IS THE DICTIONARY
-print 'reading in geneome'
-genome = open("./data/GRCh38.p7.genome.fa").read()
-print 'done reading in genome'
-chrom_header_seq = genome.split(">")[1:26]
-#TODO: make a toy of genome chromosome 22, and get 1 forward and 1 reverse from chr 22
-for g,val in d.items():
-	print g, val
-	chromosome = val[1]
-	transcripts = val[3]
-	for t,val in transcripts.items():
-		strand = val[0]
-		CDSs = val[2]
-		for CDS,val in CDSs.items():
-			start_coord = int(val[1]["abs_start"])
-			end_coord = int(val[1]["abs_end"])
-			for i in chrom_header_seq:
-				if i.split(" ")[0] == chromosome and strand == "+":
-					i = i[7:]
-					# TODO! (change! not always 7. split by \n then get first index out of list)
-					a = i.split("\n")
-					i = "".join(a)
-					val[0] = i[start_coord-1:end_coord]
-				elif i.split(" ")[0] == chromosome and strand == "-":
-					i = i[7:]
-					a = i.split("\n")
-					i = "".join(a)
-					val[0] = reverse_complement(i[start_coord-1:end_coord])
-	#NOTE! The way it is set up does not include the stop codon. Is there a way to add +3 to the end_coord only
-	#      for the last CDS?
+CDS = "ATGAGCACAGGCCTGCGGTACAAGAGCAAGCTGGCGACCCCAGGTGAGGACAAGCAGGTAGACATTGACAAGCAGTACGTGGGCTTCGCCACACTGCCCAACCAGGTGCACCGCAAGTCGGTGAAGAAAGGCTTTGACTTCACACTCATGGTGGCTGGTGGTGAGTCAGGCCTGGGGAAGTCCACACTGGTCCACAGCCTCTTCCTGACAGACTTGTACAAGGACCGGAAGCTGCTCAGTGCTGAGGGTGAGCGCATCAGCCAGACGGTAGAGATTCTAAAACACACGGTGGACATTGAGGAGAAGGGAGTCAAGCTGAAGCTCACCATCGTGGACACGCCGGGATTCGGGGACGCTGTCAACAACACCGAGTGGTGCTGGAAGCCCATCACCGACTATGTGGACCAGCAGTTTGAGCAGTACTTCCGTGATGAGAGCGGCCTCAACCGAAAGAACATCCAAGACAACCGAGTGCACTGCTGCCTATACTTCATCTCCCCCTTCGGGCATGGGTGGCTGCGGCCAGTGGATGTGGGTTTCATGAAGGCATTGCATGAGAAGGTCAACATCGTGCCTCTCATCGCCAAAGCTGACTGTCTTGTCCCCAGTGAGATCCGGAAGCTGAAGGAGCGGGTGATCCGGGAGGAGATTGACAAGTTTGGGATCCATGTATACCAGTTCCCTGAGTGTGACTCGGACGAGGATGAGGACTTCAAGCAGCAGGACCGGGAACTGAAGGTGGAGAGCGCGCCCTTCGCCGTTATAGGCAGCAACACGGTGGTGGAGGCCAAGGGGCAGCGGGTCCGGGGCCGACTGTACCCCTGGGGGATCGTGGAGGGTGTGGAGAACCAGGCGCATTGCGACTTCGTGAAGCTGCGCAACATGCTCATCCGCACGCATATGCACGACCTCAAGGACGTGACGTGCGACGTGCACTACGAGAACTACCGCGCGCACTGCATCCAGCAGATGACCAGGTGCAAACTGACCCAGGACAGCCGCATGGAGAGCCCCATCCCGATCCTGCCGCTGCCCACCCCGGACGCCGAGACTGAGAAGCTTATCAGGATGAAGGATGAGGAAGTACTGAGGCGCATGCAGGAGATGCTGCAGAGGATGAAGCAGCAGATGCAGGACCAGTGA"
+print functions.reverse_complement(CDS)
+
+if 0:
+	#Extract CDS sequences from hg38 and add to data structure
+	#MAKE INTO A FUNCTION! INPUT IS THE DICTIONARY
+	print 'reading in geneome'
+	genome = open("./data/GRCh38.p7.genome.fa").read()
+	print 'done reading in genome'
+	chrom_header_seq = genome.split(">")[1:26]
+	#TODO: make a toy of genome chromosome 22, and get 1 forward and 1 reverse from chr 22
+	for g,val in d.items():
+		print g, val
+		chromosome = val[1]
+		transcripts = val[3]
+		for t,val in transcripts.items():
+			strand = val[0]
+			CDSs = val[2]
+			for CDS,val in CDSs.items():
+				start_coord = int(val[1]["abs_start"])
+				end_coord = int(val[1]["abs_end"])
+				for i in chrom_header_seq:
+					if i.split(" ")[0] == chromosome and strand == "+":
+						i = i[7:]
+						# TODO! (change! not always 7. split by \n then get first index out of list)
+						a = i.split("\n")
+						i = "".join(a)
+						val[0] = i[start_coord-1:end_coord]
+					elif i.split(" ")[0] == chromosome and strand == "-":
+						i = i[7:]
+						a = i.split("\n")
+						i = "".join(a)
+						val[0] = reverse_complement(i[start_coord-1:end_coord])
+		#NOTE! The way it is set up does not include the stop codon. Is there a way to add +3 to the end_coord only
+		#      for the last CDS?
