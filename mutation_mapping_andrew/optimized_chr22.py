1fa94a79577b7ce488aa8d32eb34957273f6515f
Andrew Goldfarb <andrewgoldfarb@piaguest-10-239-176-184.piaguest.partners.org>
2017-02-28 14:56:22 -0500

    This is a modified file of "data_structure.py" that is optimized to work with a toy file of one forward and one reverse transcript from chromosome 22. This is important because I have updated the design to concatenate the CDS's for a given transcript and include the concatenated sequence into the data structure!

---
+++ mutation_mapping_andrew/optimized_chr22.py
@@ -0,0 +1,128 @@
+"""
+Andrew Goldfarb
+02/23/2017
+Task: Create a data structure that organizes the "gencode.v25.annotation.gtf" file by genes, transcripts, and CDS.
+Optimized for chromosome 22 toy
+"""
+#Andrew's comment
+import pickle
+import os
+import functions
+
+# sublime text editor runs from active directory in finder
+# make working directory one where script is located
+abspath = os.path.abspath(__file__)
+dname = os.path.dirname(abspath)
+os.chdir(dname)
+
+d_pickle = 'toy_gtf_dict.p'
+if not os.path.exists(d_pickle):
+	# Create the data structure and fill it with information from "gencode.v25.annotation.gtf"
+	d = {}
+	# for line in open("./data/gencode.v25.annotation.gtf", "r"):
+	for line in open("chr22_toy.gtf"):
+		if line[0] != "#":
+			fields = line.strip().split("\t")
+			chrom, annot_source, feature, start, end, dot1, strand, dot2, flag = fields
+
+			if feature == "gene" and "protein_coding" in flag:
+				words = flag.split(" ")
+				ENSG = words[1][1:-2]
+				gene_name = words[7][1:-2]
+				if ENSG not in d:
+					d[ENSG] = [gene_name, chrom, {"start": start, "end": end}, {}]
+
+			elif feature == "transcript" and "protein_coding" in flag:
+				words = flag.split(" ")
+				ENST = words[3][1:-2]
+				ENSG = words[1][1:-2]
+				if ENSG in d:
+					ensts = d[ENSG][3]
+					if ENST not in ensts:
+						d[ENSG][3][ENST] = [strand, "", {"start": start, "end": end}, {}]
+					else:
+						print "error"
+
+			elif feature == "CDS" and "protein_coding" in flag:
+				words = flag.split(" ")
+				CDS_index = int(words[17][0:-1])
+				ENST = words[3][1:-2]
+				ENSG = words[1][1:-2]
+				if ENSG in d:
+					cdss = d[ENSG][3][ENST][3]
+					if CDS_index not in cdss:
+						d[ENSG][3][ENST][3][CDS_index] = ["", {"abs_start": start, "abs_end": end}, {}]
+
+	pickle.dump(d, open('toy_gtf_dict.p', 'wb'))
+else:
+	d = pickle.load(open('toy_gtf_dict.p'))
+	print "test1"
+
+
+#CDS = "ATGAGCACAGGCCTGCGGTACAAGAGCAAGCTGGCGACCCCAGGTGAGGACAAGCAGGTAGACATTGACAAGCAGTACGTGGGCTTCGCCACACTGCCCAACCAGGTGCACCGCAAGTCGGTGAAGAAAGGCTTTGACTTCACACTCATGGTGGCTGGTGGTGAGTCAGGCCTGGGGAAGTCCACACTGGTCCACAGCCTCTTCCTGACAGACTTGTACAAGGACCGGAAGCTGCTCAGTGCTGAGGGTGAGCGCATCAGCCAGACGGTAGAGATTCTAAAACACACGGTGGACATTGAGGAGAAGGGAGTCAAGCTGAAGCTCACCATCGTGGACACGCCGGGATTCGGGGACGCTGTCAACAACACCGAGTGGTGCTGGAAGCCCATCACCGACTATGTGGACCAGCAGTTTGAGCAGTACTTCCGTGATGAGAGCGGCCTCAACCGAAAGAACATCCAAGACAACCGAGTGCACTGCTGCCTATACTTCATCTCCCCCTTCGGGCATGGGTGGCTGCGGCCAGTGGATGTGGGTTTCATGAAGGCATTGCATGAGAAGGTCAACATCGTGCCTCTCATCGCCAAAGCTGACTGTCTTGTCCCCAGTGAGATCCGGAAGCTGAAGGAGCGGGTGATCCGGGAGGAGATTGACAAGTTTGGGATCCATGTATACCAGTTCCCTGAGTGTGACTCGGACGAGGATGAGGACTTCAAGCAGCAGGACCGGGAACTGAAGGTGGAGAGCGCGCCCTTCGCCGTTATAGGCAGCAACACGGTGGTGGAGGCCAAGGGGCAGCGGGTCCGGGGCCGACTGTACCCCTGGGGGATCGTGGAGGGTGTGGAGAACCAGGCGCATTGCGACTTCGTGAAGCTGCGCAACATGCTCATCCGCACGCATATGCACGACCTCAAGGACGTGACGTGCGACGTGCACTACGAGAACTACCGCGCGCACTGCATCCAGCAGATGACCAGGTGCAAACTGACCCAGGACAGCCGCATGGAGAGCCCCATCCCGATCCTGCCGCTGCCCACCCCGGACGCCGAGACTGAGAAGCTTATCAGGATGAAGGATGAGGAAGTACTGAGGCGCATGCAGGAGATGCTGCAGAGGATGAAGCAGCAGATGCAGGACCAGTGA"
+#print functions.reverse_complement(CDS)
+
+print "test2"
+#Extract CDS sequences from hg38 and add to data structure
+#MAKE INTO A FUNCTION! INPUT IS THE DICTIONARY
+#See which step is taking too long
+
+genome_pickle = 'genome_string.p'
+if not os.path.exists(genome_pickle):
+	genome = open('GRCh38.p7.genome.fa').read()
+	chrom_header_seq = genome.split(">")[1:26]
+	for i in chrom_header_seq:
+		if "chr22 " in i:
+			chr22 = i
+	pickle.dump(chr22, open('genome_string.p', 'wb'))
+else:
+	chr22 = pickle.load(open('genome_string.p'))
+	print "test3"
+ 
+#TODO: change the names of "val" below to make them different!
+CDS_pickle = 'CDS_string_chr22.p'
+if not os.path.exists(CDS_pickle):
+	for g,val_g in d.items():
+		chromosome = val_g[1]
+		transcripts = val_g[3]
+		for t,val_t in transcripts.items():
+			full_CDS = ""
+			strand = val_t[0]
+			CDSs = val_t[3]
+			for CDS,val_c in CDSs.items():
+				Chr22 = chr22
+				start_coord = int(val_c[1]["abs_start"])
+				end_coord = int(val_c[1]["abs_end"])
+				#for i in chrom_header_seq:
+					#if i.split(" ")[0] == chromosome and strand == "+":
+						#i = i[7:]
+						## TODO! (change! not always 7. split by \n then get first index out of list)
+						#a = i.split("\n")
+						#i = "".join(a)
+						#val[0] = i[start_coord-1:end_coord]
+			#if chr22.split(" ")[0] == chromosome and strand == "+":
+				if strand == "+":
+					Chr22 = Chr22.split("\n")[1:]
+					Chr22 = "\n".join(Chr22)
+					a = Chr22.split("\n")
+					Chr22 = "".join(a)
+					val_c[0] = Chr22[start_coord-1:end_coord]
+					full_CDS = full_CDS + val_c[0]
+					#print full_CDS
+					Chr22 = chr22
+			#elif chr22.split(" ")[0] == chromosome and strand == "-":
+				if strand == "-":
+					Chr22 = Chr22.split("\n")[1:]
+					Chr22 = "\n".join(Chr22)
+					a = Chr22.split("\n")
+					Chr22 = "".join(a)
+					val_c[0] = functions.reverse_complement(Chr22[start_coord-1:end_coord])
+					full_CDS = full_CDS + val_c[0]
+					Chr22 = chr22
+			d[g][3][t][1] = full_CDS
+	pickle.dump(d, open('CDS_string_chr22.p', 'wb'))
+	print "test4"
+else:
+	d = pickle.load(open('CDS_string_chr22.p'))
+	print "test5"
+print d
