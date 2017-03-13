0aeac98f40c607153d78488873e6bbd09d6f6802
Andrew Goldfarb <andrewgoldfarb@piaguest-10-239-188-42.piaguest.partners.org>
2017-03-13 12:14:51 -0400

    Updated to include descriptions of the new functions.

--- mutation_mapping_andrew/functions.py
+++ mutation_mapping_andrew/functions.py
@@ -1,10 +1,9 @@
 """
 Andrew Goldfarb
-03/01/2017
+03/13/2017
 Listing all of the functions that I will use for mutational mapping
 """
-#Function for translating a sequence
-#Keep lines under 80 characters!!! Use parentheses
+
 def translate_cds(sequence):
 
 	"""Function: tranlsate a given nucleotide sequence into an amino acid sequence.
@@ -51,14 +50,8 @@ def translate_cds(sequence):
 			break
 	return protein
 
-# CDS = "ATGAGCACAGGCCTGCGGTACAAGAGCAAGCTGGCGACCCCAGGTGAGGACAAGCAGGTAGACATTGACAAGCAGTACGTGGGCTTCGCCACACTGCCCAACCAGGTGCACCGCAAGTCGGTGAAGAAAGGCTTTGACTTCACACTCATGGTGGCTGGTGGTGAGTCAGGCCTGGGGAAGTCCACACTGGTCCACAGCCTCTTCCTGACAGACTTGTACAAGGACCGGAAGCTGCTCAGTGCTGAGGGTGAGCGCATCAGCCAGACGGTAGAGATTCTAAAACACACGGTGGACATTGAGGAGAAGGGAGTCAAGCTGAAGCTCACCATCGTGGACACGCCGGGATTCGGGGACGCTGTCAACAACACCGAGTGGTGCTGGAAGCCCATCACCGACTATGTGGACCAGCAGTTTGAGCAGTACTTCCGTGATGAGAGCGGCCTCAACCGAAAGAACATCCAAGACAACCGAGTGCACTGCTGCCTATACTTCATCTCCCCCTTCGGGCATGGGTGGCTGCGGCCAGTGGATGTGGGTTTCATGAAGGCATTGCATGAGAAGGTCAACATCGTGCCTCTCATCGCCAAAGCTGACTGTCTTGTCCCCAGTGAGATCCGGAAGCTGAAGGAGCGGGTGATCCGGGAGGAGATTGACAAGTTTGGGATCCATGTATACCAGTTCCCTGAGTGTGACTCGGACGAGGATGAGGACTTCAAGCAGCAGGACCGGGAACTGAAGGTGGAGAGCGCGCCCTTCGCCGTTATAGGCAGCAACACGGTGGTGGAGGCCAAGGGGCAGCGGGTCCGGGGCCGACTGTACCCCTGGGGGATCGTGGAGGGTGTGGAGAACCAGGCGCATTGCGACTTCGTGAAGCTGCGCAACATGCTCATCCGCACGCATATGCACGACCTCAAGGACGTGACGTGCGACGTGCACTACGAGAACTACCGCGCGCACTGCATCCAGCAGATGACCAGGTGCAAACTGACCCAGGACAGCCGCATGGAGAGCCCCATCCCGATCCTGCCGCTGCCCACCCCGGACGCCGAGACTGAGAAGCTTATCAGGATGAAGGATGAGGAAGTACTGAGGCGCATGCAGGAGATGCTGCAGAGGATGAAGCAGCAGATGCAGGACCAGTGA"
-# print translate_cds(CDS)
 
 
-
-
-#Function for getting the reverse complement of a sequence
-#Keep lines under 80 characters!!! Use parentheses
 def reverse_complement(sequence):
 
 	"""Function: generate the reverse complement for a given nucleotide sequence
@@ -83,19 +76,34 @@ def reverse_complement(sequence):
 		reverse_seq += complements.get(s, s)
 	return reverse_seq
 
+
+
 def complement(sequence):
+
+	"""Function: generate the complement for a given nucleotide sequence
+
+	Argument:
+	sequence -- a nucleotide sequence
+
+	Output:
+	complement_seq -- the complement of the inputted sequence
+
+	For mutational mapping:
+	The annotated genome "GRCh38.p7.genome.fa" provides only the positive-strand sequence.
+	But some CDS sequences that we want to extract are on the negative strand.
+	For reporting the reference nucleotide at the position of the mutation, the negative
+	strand's reference nucleotide is really the complement of the nucleotide at the
+	reported absolute position."""
+
+
 	complements = {"A":"T", "C":"G", "G":"C", "T":"A", "a":"t", "c":"g", "g":"c", "t":"a"}
 	complement_seq = ""
 	for s in sequence:
 		 complement_seq += complements.get(s, s)
 	return complement_seq
-#CDS = "ATGAGCACAGGCCTGCGGTACAAGAGCAAGCTGGCGACCCCAGGTGAGGACAAGCAGGTAGACATTGACAAGCAGTACGTGGGCTTCGCCACACTGCCCAACCAGGTGCACCGCAAGTCGGTGAAGAAAGGCTTTGACTTCACACTCATGGTGGCTGGTGGTGAGTCAGGCCTGGGGAAGTCCACACTGGTCCACAGCCTCTTCCTGACAGACTTGTACAAGGACCGGAAGCTGCTCAGTGCTGAGGGTGAGCGCATCAGCCAGACGGTAGAGATTCTAAAACACACGGTGGACATTGAGGAGAAGGGAGTCAAGCTGAAGCTCACCATCGTGGACACGCCGGGATTCGGGGACGCTGTCAACAACACCGAGTGGTGCTGGAAGCCCATCACCGACTATGTGGACCAGCAGTTTGAGCAGTACTTCCGTGATGAGAGCGGCCTCAACCGAAAGAACATCCAAGACAACCGAGTGCACTGCTGCCTATACTTCATCTCCCCCTTCGGGCATGGGTGGCTGCGGCCAGTGGATGTGGGTTTCATGAAGGCATTGCATGAGAAGGTCAACATCGTGCCTCTCATCGCCAAAGCTGACTGTCTTGTCCCCAGTGAGATCCGGAAGCTGAAGGAGCGGGTGATCCGGGAGGAGATTGACAAGTTTGGGATCCATGTATACCAGTTCCCTGAGTGTGACTCGGACGAGGATGAGGACTTCAAGCAGCAGGACCGGGAACTGAAGGTGGAGAGCGCGCCCTTCGCCGTTATAGGCAGCAACACGGTGGTGGAGGCCAAGGGGCAGCGGGTCCGGGGCCGACTGTACCCCTGGGGGATCGTGGAGGGTGTGGAGAACCAGGCGCATTGCGACTTCGTGAAGCTGCGCAACATGCTCATCCGCACGCATATGCACGACCTCAAGGACGTGACGTGCGACGTGCACTACGAGAACTACCGCGCGCACTGCATCCAGCAGATGACCAGGTGCAAACTGACCCAGGACAGCCGCATGGAGAGCCCCATCCCGATCCTGCCGCTGCCCACCCCGGACGCCGAGACTGAGAAGCTTATCAGGATGAAGGATGAGGAAGTACTGAGGCGCATGCAGGAGATGCTGCAGAGGATGAAGCAGCAGATGCAGGACCAGTGA"
-#print complement(CDS)
 
 
 
-
-#Function to create and populate the data structure containing gene annotation information.
 def populate_gene_annot_struc(gencode_file):
 
 	"""Function: Create the data structure and fill it with information from "gencode.v25.annotation.gtf"
@@ -149,12 +157,9 @@ def populate_gene_annot_struc(gencode_file):
 					if CDS_index not in cdss:
 						d[ENSG][3][ENST][2][CDS_index] = ["", {"abs_start": start, "abs_end": end}]
 	return d
-#print populate_gene_annot_struc("chr22_toy.gtf")
 
 
 
-
-#Function to filter "HGMD_allmut.tsv" file for lines that contain missense/nonsense mutations.
 def filter_for_coding_variants(mutation_file):
 
 	"""Function: Filter the HGMD file "HGMD_allmut.tsv" for lines that include data for point/nonsense mutations only.
@@ -178,13 +183,9 @@ def filter_for_coding_variants(mutation_file):
 		if fields[7] == "NULL" and fields[8] == "NULL" and fields[6] != "NULL":
 			lines_w_point_mutations.append(line)
 	return lines_w_point_mutations
-#print len(filter_for_coding_variants("./data/HGMD_allmut.tsv"))
 
 
 
-
-#Function to create and populate the data structure containing HGMD mutation information.
-#Keep lines under 80 characters!!! Use parentheses
 def populate_variant_struc(lines_w_point_mutations):
 
 	"""Function: Create and populate a data structure containing HGMD point mutation information.
@@ -207,20 +208,39 @@ def populate_variant_struc(lines_w_point_mutations):
 	mutations_dict = {}
 	for line in lines_w_point_mutations:
 		fields = line.strip().split("\t")
-		disease, gene, chrom, genename, gdbid, omimid, amino, deletion, insertion, codon, codonAff, descr, hgvs, hgvsAll, dbsnp, chromosome, startCoord, endCoord, tag, author, fullname, allname, vol, page, year, pmid, reftag, comments, acc_num, new_date, base = fields
+		(disease, gene, chrom, genename, gdbid, omimid, amino, deletion, insertion, codon, codonAff, descr, 
+			hgvs, hgvsAll, dbsnp, chromosome, startCoord, endCoord, tag, author, fullname, allname, vol, page, 
+			year, pmid, reftag, comments, acc_num, new_date, base) = fields
 		ref_nt = hgvs[-3]
 		mut_nt = hgvs[-1]
 		ref_AA = hgvsAll.split(" ")[-1][0]
 		mut_AA = hgvsAll.split(" ")[-1][-1]
-		mutations_dict[gene + " " + startCoord] = {"disease": disease, "gene": gene, "chromosome": chromosome, "coordinate":startCoord, "ref_nt": ref_nt, "mut_nt": mut_nt, "ref_AA": ref_AA, "mut_AA": mut_AA}
+		mutations_dict[gene + " " + startCoord] = ({"disease": disease, "gene": gene, "chromosome": chromosome, 
+			"coordinate":startCoord, "ref_nt": ref_nt, "mut_nt": mut_nt, "ref_AA": ref_AA, "mut_AA": mut_AA})
 	return mutations_dict
-#print populate_variant_struc(filter_for_coding_variants("./data/HGMD_allmut.tsv"))["A2ML1 8851954"]
+
+
 
 def create_mutation_dictionary(mutation_file):
+
+	"""Function: Create and populate the HGMD data structure, given a set of filtered lines.
+		Basically condenses the functions "filter_for_coding_variants" and "populate_variant_struc"
+		into one.
+
+	Argument:
+		mutation_file -- a file input that contains all mutational information associated with disease
+					Function is specific to HGMD format, such as "HGMD_allmut.tsv"
+
+	Output:
+	a dictionary, containing the data structure that summarizes HGMD mutation information.
+
+	For mutational mapping:
+	First, the lines from "HGMD_allmut.tsv" containing missense/nonsense mutations are filtered into a list by
+	the "filter_for_coding_variants" function, which outputs a list of these lines. In the current function,
+	"populate_variant_struc", the input is this list. The function goes through the list, extracting relevant
+	information and organizing it into a dictionary, the output."""
+
 	return populate_variant_struc(filter_for_coding_variants(mutation_file))
-#print create_mutation_dictionary("./data/HGMD_allmut.tsv")["A2ML1 8851954"]
-#Example:  [{'ref_AA': 'Arg', 'coordinate': '8851954', 'mut_AA': 'Leu', 'ref_nt': 'G', 'gene': 'A2ML1', 'disease': 'Noonan-like syndrome', 'chromosome': '12', 'mut_nt': 'T'}]
-#Function: Load genome sequence data into a dictionary: key = "chr#", value = sequence
 
 
 
@@ -254,56 +274,175 @@ def load_reference_genome(genome_file):
 		chrom_seq = seq
 		genome_dict[chrom_key] = chrom_seq
 	return genome_dict
-#print len(load_reference_genome("GRCh38.p7.genome.fa"))
 
 
 
 def extract_cds_sequence_from_genome(chromosome, start_coord, end_coord, genome_dict):
 
+	"""Function: Extracts a sequence from GRCh38.p7.genome.fa dictionary (genome_dict) created 
+			in the "load_reference_genome" function, given start and end coordinates, and the chromosome.
+
+		Argument:
+		chromosome -- the chromosome "key" for the genome_dict dictionary
+		start_coord -- the start coordinate of a sequence to be extracted
+		end_coord -- the end coordinate of a sequence to be extracted
+		genome_dict -- a dictionary that organizes the fasta file, GRCh38.p7.genome.fa, where the key
+		is "chr#" and the value is the sequence for that chromosome
+
+		Output:
+		extracted_sequence -- the extracted sequence with the defined coordinates of that chromosome
+
+		For mutational mapping:
+		This function is performed in order to fill the data structure created in the "populate_gene_annot_struc"
+		function. The "raw" positive-stranded CDS sequences are extracted and put into the data structure. """
+
 	chrom_sequence = genome_dict[chromosome]
 	extracted_sequence = chrom_sequence[start_coord-1:end_coord]
 	return extracted_sequence
-# #NOTE! In main loop, if strand is negative, do reverse complement of output of above function.
+
 
 
 def create_table(table_name):
+
+	"""Function: Creates the tab-delimited table that will contain final information. Also, writes in the 
+	"header" information that summarizes what is in the table. 
+
+		Argument:
+		table_name -- the name you wish to call the table. Put into quotes!
+
+		Output:
+		table_file -- the variable that the table is saved as
+
+		For mutational mapping:
+		Table header includes: (1) the disease the mutation is associated with, (2) the gene name, as reported
+		by HGMD, (3) the gene name, as reported by gencode, (4) the ENSG, from gencode, (5) the chromosome,
+		from gencode, (6) the mutation coordinate, from HGMD, (7) the strand ("+" or "-"), from gencode,
+		(8) the ENST, from gencode, (9), gencode's reference nucleotide at the position that HGMD reports
+		a mutation, (10) the reference nucleotide as reported by HGMD, (11) the alternative nucleotide, as
+		reported by HGMD, (12) the reference amino acid, as translated by my script, (13) the reference amino
+		acid, reported by HGMD, (14) the alternative amino acid, as translated by my script, (15) the alternative
+		amino acid, as reported by HGMD, (16) the relative nucleotide position of the mutation within the CDS,
+		(17) the relative amino acid position of the mutation in the translated CDS, (18) the full-length coding
+		sequence in nucleotides, with the mutation designated in lower case, (19) the full-length translated
+		coding sequence in amino acids, with the altered amino acid designated in lower case."""
+
 	table_file = open(table_name, "w")
-	table_file.write("disease" + "\t" + "hg_gene_name" + "\t" + "gc25_gene_name" + "\t" + "ENSG" + "\t" + "chromosome" + "\t" + "coordinate" + "\t" + "strand" + "\t" + "ENST" + "\t" + "gc25_ref_nt" + "\t" + "hg_ref_nt" + "\t" + "hg_alt_nt" + "\t" + "gc25_ref_aa" + "\t" + "hg_ref_aa" + "\t" + "gc25_alt_aa" + "\t" + "hg_alt_aa" + "\t" + "nt_mut_relative_position" + "\t" + "aa_mut_relative_position" + "\t" + "cds_seq_alt" + "\t" + "prot_seq_alt" + "\n")
+	table_file.write(("disease" + "\t" + "hg_gene_name" + "\t" + "gc25_gene_name" + "\t" + "ENSG" + "\t" + 
+		"chromosome" + "\t" + "coordinate" + "\t" + "strand" + "\t" + "ENST" + "\t" + "gc25_ref_nt" + "\t" + 
+		"hg_ref_nt" + "\t" + "hg_alt_nt" + "\t" + "gc25_ref_aa" + "\t" + "hg_ref_aa" + "\t" + "gc25_alt_aa" + 
+		"\t" + "hg_alt_aa" + "\t" + "nt_mut_relative_position" + "\t" + "aa_mut_relative_position" + "\t" + 
+		"cds_seq_alt" + "\t" + "prot_seq_alt" + "\n"))
 	return table_file
-#create_table("check.txt")
+
+
 
 def concatenate(full_seq, seq_to_be_added):
+	
+	"""Function: Concatenates sequences. 
+
+		Argument:
+		full_seq -- the final sequence to be constructed
+		seq_to_be_added -- the sequence to be added to construct a final sequence
+
+		Output:
+		full_seq -- the concatenated sequence
+
+		For mutational mapping:
+		This function is meant to concatenate individual CDS sequences together to construct a full-length
+		coding sequence for a given transcript. """
+
 	full_seq = full_seq + seq_to_be_added
 	return full_seq
 
+
+
 def rel_start_func(rel_start, rel_end):
+		
+	"""Function: To updated and return the relative start position of a string
+
+		Argument:
+		rel_start -- the relative start position
+		rel_end -- the relative end position
+
+		Output:
+		rel_start -- the updated relative start position
+
+		For mutational mapping:
+		This function is performed to help get the relative position of the mutation in a full-length CDS. 
+		As each CDS is concatenated the start position is 1 position after length of the previously
+		concatenated sequence"""
+
 	rel_start = rel_end + 1
 	return rel_start
 
+
+
 def rel_end_func(rel_start, rel_end, string):
+		
+	"""Function: To updated and return the relative end position of a string
+
+		Argument:
+		rel_start -- the relative start position
+		rel_end -- the relative end position
+		string -- the sequence being concatenated
+
+		Output:
+		rel_end -- the updated relative end position
+
+		For mutational mapping:
+		This function is performed to help get the relative position of the mutation in a full-length CDS. 
+		As each CDS is concatenated, the end position is the previously determined relative start position,
+		plus the length of the sequence that is added."""
+
 	rel_end = rel_start + len(string) - 1
 	return rel_end
 
+
+
 def mutation_rel_position(rel_start, rel_end, difference):
+
+	"""Function: To get the relative position of the mutation within the coding sequence.
+
+		Argument:
+		rel_start -- the relative start position
+		rel_end -- the relative end position
+		difference -- the difference between the mutation coordinate and relative start position. This value
+			is equivalent to the index of the position of the mutation in the string sequence
+
+		Output:
+		mutation_rel_position -- a number, indicating the relative position within the CDS of where the mutation
+		is positioned. This is assuming that the first index is 1, not 0.
+
+		For mutational mapping:
+		The relative position of the mutation within the CDS is printed out into the final table. We want to know this
+		to help us be more confident where the mutation is when designing primers."""
+
+
 	mutation_rel_position = (range(rel_start, rel_end+1))[difference]
 	return mutation_rel_position
 	
+
+
 def replace_ref_nt_w_alt_nt(raw_cds_seq, difference, alt_nt):
+	
+	"""Function: To replace the reference nucleotide with the reported mutation in lower case.
+
+		Argument:
+		raw_cds_seq -- the "raw" extracted CDS from GRCh38.p7.genome.fa, with each nucleotide being an index
+			in a list.
+		difference -- the difference between the mutation coordinate and relative start position. This value
+			is equivalent to the index of the position of the mutation in the string sequence
+		alt_nt -- the nucleotide after the mutation, as reported by HGMD.
+
+		Output:
+		mutated_cds_seq -- the "raw" extracted CDS, but the index where the mutation is in is changed to
+			the mutation nucleotide as reported by HGMD, but in lowercase.
+
+		For mutational mapping:
+		This functions changes the reference nucleotide to the alternative nucleotide, and makes it lowercase,
+		so that it can be identified."""
+
 	raw_cds_seq[difference] = alt_nt.lower()
 	mutated_cds_seq = "".join(raw_cds_seq)
 	return mutated_cds_seq
-#mutated_cds_seq = functions.replace_ref_net_w_alt_nt(d[g][3][t][2][CDS][0], mutation_dict[m]["coordinate"], d[g][3][t][2][CDS][1]["abs_start"], mutation_dict[m]["mut_nt"])
 
-def get_gc_ref_nt(raw_cds, mutation_coord, CDS_start):
-	raw_cds_seq = list(raw_cds)
-	difference = int(int(mutation_coord) - int(CDS_start))
-	gencode_ref_nt = raw_cds_seq[difference]
-	return gencode_ref_nt
-#gencode_ref_nt = functions.get_gc_ref_nt(d[g][3][t][2][CDS][0], mutation_dict[m]["coordinate"], d[g][3][t][2][CDS][1]["abs_start"])
-
-
-
-
-
-# # **********GS_TODO: if time, add tester function, rather than now-commented out lines
-# # need to sit and discuss before staring
