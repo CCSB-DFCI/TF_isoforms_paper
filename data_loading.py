92a6e37d97c0bcc655deadd330896beb486c589e
Luke Lambourne <lambourne.luke@gmail.com>
2019-10-16 11:22:20 -0400

    Move data loading to seperate file

---
+++ data_loading.py
@@ -0,0 +1,136 @@
+import numpy as np
+import pandas as pd
+
+import ccsblib
+
+
+def load_valid_isoform_clones():
+    """The subset of TF isoform clones that map to GenCode?????
+
+    Returns:
+        set(str): clone accession IDs
+
+    """
+    return set(pd.read_sql("""SELECT unique_acc
+                                FROM tf_screen.iso6k_annotation;""",
+                           ccsblib.paros_connection())
+                           ['unique_acc']
+                           .values)
+
+
+def load_tf_isoform_screen_results():
+    """There were two screens performed:
+    
+    The cloned TF isoforms as AD-fusions against DB-fusions of:
+    (1) ORFeome 9.1
+    (2) Subset of TFs and co-factors
+    
+    Returns:
+        pandas.DataFrame: for each pair, was it found in the first and second screens
+        
+    """
+    qry = """SELECT ad_orf_id,
+                    db_orf_id,
+                    pool_name 
+               FROM swimseq_run.INGS_IST a,
+                    swimseq_run.NGS_POOL b 
+              WHERE a.pool_id = b.pool_id 
+                AND a.pool_id in (787, 788)  
+                AND ist_score>=0.2;"""
+    df = pd.read_sql(qry, ccsblib.paros_connection())
+    df = (pd.get_dummies(df, columns=['pool_name']).groupby(['ad_orf_id', 'db_orf_id']).sum() > 0).reset_index()
+    df = df.rename(columns={'pool_name_ds20180213_TFisoS04': 'in_orfeome_screen',
+                            'pool_name_ds20180213_TFisoS05': 'in_focussed_screen'})
+    if not (df['in_orfeome_screen'] | df['in_focussed_screen']).all():
+        raise UserWarning('Something went wrong...')
+    return df
+
+
+def load_isoform_and_paralog_y2h_data():
+    """
+    - NS: sequencing failed
+    - NC: no call
+    
+    """
+    valid_clones = load_valid_isoform_clones()
+    qry_a = """select a.category,
+                      a.ad_orf_id,
+                      b.unique_acc AS ad_clone_acc,
+                      a.ad_symbol AS ad_gene_symbol,
+                      a.db_orf_id,
+                      c.symbol AS db_gene_symbol,
+                      a.final_score AS score
+                 FROM tf_screen.tf_isoform_final AS a 
+                 LEFT JOIN tf_screen.iso6k_sequences AS b 
+                   ON a.ad_orf_id = b.orf_id 
+                 LEFT JOIN horfeome_annotation_gencode27.orf_class_map_ensg AS c 
+                   ON a.db_orf_id = c.orf_id;"""
+    df_a = pd.read_sql(qry_a, ccsblib.paros_connection())
+    df_a = df_a.loc[df_a['ad_clone_acc'].isin(valid_clones), :]
+    
+    # remove duplicate ORF for gene DDX39B, where sequencing mostly failed
+    df_a = df_a.loc[df_a['db_orf_id'] != 3677, :]
+    
+    df_a['category'] = df_a['category'].map({'ppi': 'tf_isoform_ppis',
+                                             'ng_stem_cell_factor': 'tf_isoform_ppis',
+                                             'rrs': 'rrs_isoforms',
+                                             'litbm': 'lit_bm_isoforms'})
+    qry_b = """select a.simple_category AS category,
+                      a.ad_orf_id,
+                      b.unique_acc AS ad_clone_acc,
+                      a.ad_symbol AS ad_gene_symbol,
+                      a.db_orf_id,
+                      c.symbol AS db_gene_symbol,
+                      a.final_score AS score
+                 FROM tf_screen.paralog_final AS a 
+                 LEFT JOIN tf_screen.iso6k_sequences AS b 
+                   ON a.ad_orf_id = b.orf_id 
+                 LEFT JOIN horfeome_annotation_gencode27.orf_class_map_ensg AS c 
+                   ON a.db_orf_id = c.orf_id;"""
+    df_b = pd.read_sql(qry_b, ccsblib.paros_connection())
+    df_b = df_b.loc[df_b['ad_clone_acc'].isin(valid_clones), :]
+    df_b['category'] = df_b['category'].map({'paralog': 'tf_paralog_ppis',
+                                             'PDI_PPI': 'paralog_with_PDI',
+                                             'nonparalog': 'non_paralog_control',
+                                             'rrs': 'rrs_paralogs',
+                                             'litbm': 'lit_bm_paralogs'})
+    df = pd.concat([df_a, df_b])
+    # drop cases where single orf ID mapped to multiple gene symbols
+    df = df.drop_duplicates(['category', 'ad_orf_id', 'db_orf_id'])
+    # drop interaction partner ORFs whose sequence does not map to an ensembl gene
+    df = df.dropna(subset=['db_gene_symbol'])
+    """
+    # Need to map the screen data to the gene level first 
+    screen = load_tf_isoform_screen_results()
+    pd.merge(y2h,
+            screen,
+            how='left',
+            on=['ad_orf_id', 'db_orf_id']).sort_values(['ad_gene_symbol', 'db_gene_symbol'])
+    """
+    return df
+
+
+def load_y1h_pdi_data():
+    df = pd.read_csv('data/a2_juan_pdi_w_unique_isoacc.tsv', sep='\t')
+    df = (pd.concat([df.loc[:, ['tf', 'unique_acc']], 
+                     pd.get_dummies(df['bait'])],
+                    axis=1)
+            .groupby(['tf', 'unique_acc']).sum() > 0).reset_index()
+    zeros = pd.read_csv('data/a2_juan_isoforms_wo_pdi.tsv', sep='\t')
+    df = pd.concat([df, zeros], axis=0, sort=False).reset_index(drop=True).fillna(False)
+    df = df.sort_values(['tf', 'unique_acc'])
+    return df
+
+
+def load_m1h_activation_data():
+    """
+    
+    
+    """
+    df = pd.read_csv('data/a_m1h_final_table.tsv', sep='\t')
+    df = df.rename(columns={'pos_acc': 'clone_acc'})
+    for column in df.columns:
+        if column.startswith('M1H_rep'):
+            df[column] = np.log2(df[column])
+    df = df.sort_values(['gene', 'clone_acc'])
+    return df
