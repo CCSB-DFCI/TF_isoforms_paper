26c3c22dec46247fa0bdd3ff239a910f786948fd
Luke Lambourne <lambourne.luke@gmail.com>
2025-03-04 13:37:31 -0500

    Renumbered supplemenatary files

--- supp/number_supplementary_files.sh
+++ supp/number_supplementary_files.sh
@@ -1,19 +1,12 @@
 #!/bin/bash
 
-cp SuppTable_CloneList.txt Table_S1.tsv
-cp SuppTable_DNABaits.txt Table_S2.tsv
-cp SuppTable_eY1HResults.txt Table_S3.tsv
-cp SuppTable_M1HResults.txt Table_S4.tsv
-cp SuppTable_PairwiseY2HResults.txt Table_S5.tsv
-cp SuppTable_N2HResults.txt Table_S6.tsv
-cp SuppTable_PDI_validation.txt Table_S7.tsv
-cp SuppTable_CREB1-PBM.txt Table_S8.tsv
-cp SuppTable_TBX5-PBM.txt Table_S9.tsv
-cp SuppTable_SinglesCloneList.txt Table_S10.tsv
-cp SuppTable_Paralogs.txt Table_S11.tsv
-cp SuppTable_Condensates.txt Table_S12.tsv
-cp SuppTable_NegRegs.txt Table_S13.tsv
-cp SuppTable_TCGASamps.txt Table_S14.tsv
-cp SuppTable_TCGAResults.txt Table_S15.tsv
-cp SuppTable_CloningPrimers.txt Table_S16.tsv
-cp SuppTable_DBD_definition.txt Table_S17.tsv
+zip Data_S1.zip SuppTable_CloneList.txt SuppTable_CloningPrimers.txt SuppTable_SinglesCloneList.txt SuppTable_DBD_definition.txt
+zip Data_S2.zip SuppTable_eY1HResults.txt SuppTable_DNABaits.txt SuppTable_PDI_validation.txt
+cp SuppTable_M1HResults.txt Data_S3.tsv
+zip Data_S4.zip SuppTable_PairwiseY2HResults.txt SuppTable_N2HResults.txt
+zip Data_S5.zip SuppTable_CREB1-PBM.txt SuppTable_TBX5-PBM.txt
+cp SuppTable_Paralogs.txt Data_S6.tsv
+cp SuppTable_Condensates.txt Data_S7.tsv
+cp SuppTable_NegRegs.txt Data_S8.tsv
+zip Data_S9.zip SuppTable_TCGASamps.txt SuppTable_TCGAResults.txt
+cp SuppTable_JoungResults.txt Data_S10.tsv
